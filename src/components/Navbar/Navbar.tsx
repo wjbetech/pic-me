698d786576d2425b40ca26a31ce9b38ed0a80e71
wjbetech <wjbetech@github.com>
2026-01-13 12:54:16 +0900

    Fixed padding issues in the Navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,9 +2,9 @@ import ThemeToggle from "../ThemeToggle/ThemeToggle";
 
 export default function Navbar() {
   return (
-    <div className="navbar shadow-sm w-full items-center align-middle">
+    <div className="navbar shadow-sm w-full items-center align-middle p-4">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl text-amber-500">PicMe</a>
+        <a className="btn btn-ghost text-xl text-amber-500 p-0">PicMe</a>
       </div>
       <div className="flex gap-4 items-center">
         <ThemeToggle />
